90a2337413d8671bbb90fb17f28f1970f70f32f1
iankhor <iankhorbc.thecoder@gmail.com>
2020-03-14 16:33:45 +1100

    example of bad methods without using interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
+import { CustomMap } from './CustomMap';
 import { User } from './User';
 import { Company } from './Company';
 
-// const user = new User();
-// const company = new Company();
+const user = new User();
+const company = new Company();
 
-new google.maps.Map(document.getElementById('map'), {
-  zoom: 1,
-  center: { lat: 0, lng: 0 }
-});
+const map = new CustomMap('map');
+
+map.addUserMarker(user);
+map.addCompanyMarker(company);
